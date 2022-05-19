e0f7eaed83971baf71f0fd2e790952eacf6f03d1
A Công <86156628+congbio@users.noreply.github.com>
2022-05-19 10:48:09 +0700

    upload file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import ReactCore from './formreactjs/cacuDBT';
 // import App from './formreactjs/ForomMenu/FormSelection';
-import App from './fetch/fetch';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import App from "./hookAPI/demo";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
